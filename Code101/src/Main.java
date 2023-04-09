71f0500ab2091f585c6ed1d690114c6f2bff51ca
mahmoodajaz2020 <mahmoodajaz2020@gmail.com>
2023-04-10 03:15:55 +0530

    new commit

--- Code101/src/Main.java
+++ Code101/src/Main.java
@@ -1,5 +1,12 @@
+import leetcode.Num54SpiralMatrix.Solution;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Solution sol= new Solution();
+        int[][] matrix= {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] matrix2={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        System.out.println(sol.spiralOrder(matrix));
+        System.out.println(sol.spiralOrder(matrix2));
+
     }
 }
