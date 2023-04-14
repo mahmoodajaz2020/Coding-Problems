8c0242350ecd59b66334df43d8421c5c5ab2146b
mahmoodajaz2020 <mahmoodajaz2020@gmail.com>
2023-04-15 01:14:10 +0530

    day commit

--- Code101/src/Main.java
+++ Code101/src/Main.java
@@ -1,19 +1,7 @@
-import leetcode.Num2309RemoveStars.RemoveStars;
-import leetcode.Num70SimplifyPath.SimplifyPath;
-import leetcode.Num73SetRowColZero.Solution;
-import leetcode.Num74SearchIn2D.SeachIn2D;
-import leetcode.Num75SortColors.SortColors;
-import leetcode.Num76MinWindowSub.MinWindowSub;
-import leetcode.Num90Subsets.Subsets2;
 
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[]nums={0,1,2,0,0,0,1,1,1};
-        SortColors sortColors= new SortColors();
-        sortColors.sortColors(nums);
-        System.out.println(Arrays.toString(nums));
 
     }
 }
