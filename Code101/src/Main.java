8fc3137db7541605897242172af50a6dfdffb783
mahmoodajaz2020 <mahmoodajaz2020@gmail.com>
2023-04-15 03:39:07 +0530

    new push

--- Code101/src/Main.java
+++ Code101/src/Main.java
@@ -1,7 +1,12 @@
+import leetcode.Num78Subsets.Subsets;
 
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        Subsets sub = new Subsets();
+        int nums[]= {0};
+        System.out.println(sub.subsets(nums));
     }
 }
